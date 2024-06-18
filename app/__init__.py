24af1f78994f9218a1987b56c154be03a8dab792
Afsaumutoniwa <afsaumutoniwa@pontemanalytics.com>
2024-06-18 16:56:12 +0200

    modifying app/__init__

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+FLASK_APP = os.getenv('FLASK_APP')
+FLASK_ENV = os.getenv('FLASK_ENV')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
+ 
 
-# Configuration for static files and templates
-app.static_folder = 'static'
-app.template_folder = 'templates'
-
-# Example route to render a template
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "Hello, World!"
 
-# Add other routes as per your application structure
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
