16b108d3ac7902a50c7f5ba830bc810e9973401a
Afsaumutoniwa <afsaumutoniwa@pontemanalytics.com>
2024-06-19 16:26:48 +0200

    modified the templates and statics to relink pages

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 from datetime import datetime
@@ -11,36 +11,43 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///smws.db')
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = SECRET_KEY
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
-db = SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
-    
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}')"
-    
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+app.template_folder = 'app/templates'
+app.static_folder = 'app/static'
 
-    def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
 
-@app.route('./templates')
+@app.route('/index.html')
 def index():
     return render_template('index.html')
 
+
+@app.route('/authentication/getstarted.html')
+def login():
+    return render_template('authentication/getstarted.html')
+
+@app.route('/authentication/getstarted.html')
+def signup():
+    return render_template('authentication/getstarted.html')
+
+@app.route('/nav/services.html')
+def services():
+    return render_template('nav/services.html')
+
+@app.route('/nav/services/wastepickup.html')
+def wastepickup():
+    return render_template('nav/services/wastepickup.html')
+
+@app.route('/nav/services/smartwm.html')
+def smartwm():
+    return render_template('nav/services/smartwm.html')
+
+@app.route('/nav/about.html')
+def about():
+    return render_template('nav/about.html')
+
+@app.route('/nav/contact.html')
+def contact():
+    return render_template('nav/contact.html')
+
 if __name__ == "__main__":
-    with app.app_context():    
-        db.create_all()
-        print("Database tables created successfully.")
-        app.run(debug=True)
+    app.run(debug=True)
