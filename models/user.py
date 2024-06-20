0771fcdc0ee65b2eb022f094f58628c850d2ef23
jkeza1 <j.keza1@alustudent.com>
2024-06-20 14:35:36 +0200

    added files

---
+++ models/user.py
@@ -0,0 +1,9 @@
+from db import db
+class usermodel(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80))
+    password = db.Column(db.String(80))
+
+  
